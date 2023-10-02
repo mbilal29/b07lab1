c667832cb8defaddfa4ce148411d2d72292bb3e5
Muhammad Bilal <muhammadbilal@Muhammads-MacBook-Pro.local>
2023-10-02 02:13:25 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,27 +1,71 @@
+import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
-	
-	public static void main(String [] args) {
-		
-		Polynomial p = new Polynomial(); 
-		
-		System.out.println(p.evaluate(3));
-		
-		double [] c1 = {6,0,0,5};
-		
-		Polynomial p1 = new Polynomial(c1);
-		
-		double [] c2 = {0,-2,0,0,-9};
-		
-		Polynomial p2 = new Polynomial(c2); 
-		
-		Polynomial s = p1.add(p2); 
-		
-		System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-		
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		
-		else
-			System.out.println("1 is not a root of s");
+	public static void main(String[] args) throws IOException {
+
+		// Create polynomials
+		double[] const1 = { 2, -3, 6 }; // 2x - 3x^3 + 6x^5
+		int[] exp1 = { 1, 3, 5 };
+		Polynomial poly1 = new Polynomial(const1, exp1);
+
+		double[] const2 = { 3, 1, -2, 4 }; // 3 + x - 2x^2 + 4x^3
+		int[] exp2 = { 0, 1, 2, 3 };
+		Polynomial poly2 = new Polynomial(const2, exp2);
+
+		double[] const3 = { 1, 1 }; // 1 + 1x^3
+		int[] exp3 = { 0, 3 };
+		Polynomial poly3 = new Polynomial(const3, exp3);
+
+		// Add polynomials
+		Polynomial sum = poly1.add(poly2);
+		System.out.println(
+				"Sum of polynomials: " + Arrays.toString(sum.constants) + "  " + Arrays.toString(sum.exponents));
+		System.out.println("Expected: [3.0, 3.0, -2.0, 1.0, 6.0]  [0, 1, 2, 3, 5]");
+		System.out.println();
+
+		// Evaluate polynomial at x = 2
+		double x = 2;
+		double result = poly1.evaluate(x);
+		System.out.println("poly1(2) = " + result); // Expected: 172.0
+		System.out.println("Expected: 172.0");
+		System.out.println();
+
+		// Check if poly1 has a root at x = 1
+		if (poly1.hasRoot(1)) {
+			System.out.println("1 is a root of poly1");
+		} else {
+			System.out.println("1 is not a root of poly1");
+		}
+		System.out.println();
+
+		// Check if poly3 has a root at x = -1
+		if (poly3.hasRoot(-1)) {
+			System.out.println("-1 is a root of poly3");
+		} else {
+			System.out.println("-1 is not a root of poly3");
+		}
+		System.out.println();
+
+		// Add polynomials
+		Polynomial product = poly1.multiply(poly2);
+		System.out.println("product of polynomials: " + Arrays.toString(product.constants) + "  "
+				+ Arrays.toString(product.exponents));
+		System.out.println("Expected: [6.0, 2.0, -13.0, 5.0, 24.0, -6.0, -12.0, 24.0]  [1, 2, 3, 5, 6, 7, 8]");
+		System.out.println();
+
+		// Save polynomial to file
+		poly1.saveToFile("poly.txt");
+		System.out.println("Saved Polynomial");
+
+		// Create polynomial from file
+		File f = new File("poly.txt");
+		Polynomial newPoly = new Polynomial(f);
+		System.out.println("Polynomial read from file: " + Arrays.toString(newPoly.constants) + "  "
+				+ Arrays.toString(newPoly.exponents));
+		System.out.println("Expected: [2.0, -3.0, 6.0]  [1, 3, 5]");
+		System.out.println();
+
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,78 +1,304 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Polynomial {
-	
-	private double[] constants;
-	
-	//no-argument constructor that sets the polynomial to zero
+
+	public double[] constants;
+	public int[] exponents;
+
+	// no-argument constructor that sets the polynomial to zero
 	public Polynomial() {
-		constants = new double[] {0};
-			
+		constants = new double[] { 0 };
+		exponents = new int[] { 0 };
 	}
-	
-	//constructor that takes an array of double as an argument and sets the coefficients accordingly
-	public Polynomial(double[] input) {
-		
-		int len = input.length;
-		
-		constants = new double[len];
-		
-		for (int i=0; i<len; i++) {
-			constants[i] = input[i];
+
+	// constructor that takes an array of double as an argument and sets the
+	// coefficients accordingly
+	public Polynomial(double[] constants, int[] exponents) {
+
+		this.constants = constants;
+		this.exponents = exponents;
+
+	}
+
+	public Polynomial(File file) throws IOException {
+
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line = br.readLine();
+
+		if (br.equals("")) {
+			constants = null;
+			exponents = null;
 		}
-	
+
+		else {
+
+			line = line.replace("+", "#+");
+			line = line.replace("-", "#-");
+
+			String[] splitPoly = line.split("#");
+
+			double[] newConst = new double[splitPoly.length];
+			int[] newExp = new int[splitPoly.length];
+
+			for (int i = 0; i < splitPoly.length; i++) {
+
+				String num = splitPoly[i];
+				int index = num.indexOf('x');
+
+				if (index != -1) {
+
+					newConst[i] = Double.parseDouble(num.substring(0, index));
+					newExp[i] = Integer.parseInt(num.substring(index + 1));
+
+				} else {
+
+					newConst[i] = Double.parseDouble(num);
+					newExp[i] = 0;
+				}
+			}
+
+			constants = newConst;
+			exponents = newExp;
+
+		}
+		br.close();
 	}
-	
-	//takes one argument of type Polynomial and returns the polynomial resulting from adding the calling object and the argument
+
+	// takes one argument of type Polynomial and returns the polynomial resulting
+	// from adding the calling object and the argument
 	public Polynomial add(Polynomial input) {
-		
-        int maxLen = Math.max(constants.length, input.constants.length);
-        
-        double[] newPoly = new double[maxLen];
-        
-        double a, b;
 
-        for (int i = 0; i < maxLen; i++) {
-        	
-        	a = 0;
-        	b = 0;
-        	
-        	if (i < constants.length) {
-        		a = constants[i];
-        	}
-        	
-        	if (i < input.constants.length) {
-        		b = input.constants[i];
-        	}
-        	
-            newPoly[i] = a + b;
-            
-        }
+		int[] hold = new int[exponents.length + input.exponents.length];
 
-        return new Polynomial(newPoly);
-    }
-		
-	
-	//takes one argument of type double representing a value of x and evaluates the polynomial accordingly
-	public double evaluate(double x) {
-		
-		double total = 0;
-		
-		for (int i=0; i<constants.length ; i++) {
-			
-			total += constants[i]*(Math.pow(x, i));
+		for (int i = 0; i < exponents.length; i++) {
+
+			hold[i] = exponents[i];
 		}
-		
+
+		int k = 0;
+
+		for (int i = exponents.length; i < hold.length; i++) {
+
+			hold[i] = input.exponents[k];
+
+			k++;
+		}
+
+		// find # of distinct values in hold[]
+		int maxExp = 0;
+
+		for (int i = 0; i < hold.length; i++) {
+
+			if (hold[i] > maxExp)
+				maxExp = hold[i];
+
+		}
+		int total = 0;
+		int count = 0;
+
+		for (int i = 0; i <= maxExp; i++) {
+
+			count = 0;
+			for (int j = 0; j < hold.length; j++) {
+
+				if (hold[j] == i)
+					count++;
+
+			}
+
+			if (count > 0)
+				total++;
+
+		}
+
+		double[] newPolyConst = new double[total];
+		int[] newPolyExp = new int[total];
+
+		k = 0;
+
+		boolean check = false;
+
+		// populate newPolyExp
+		for (int i = 0; i <= maxExp; i++) {
+
+			check = false;
+			for (int j = 0; j < hold.length; j++) {
+
+				if (hold[j] == i) {
+
+					newPolyExp[k] = i;
+					check = true;
+
+				}
+			}
+			if (check)
+				k++;
+		}
+
+		// populate newPolyConst
+		for (int i = 0; i < exponents.length; i++) {
+
+			count = 0;
+			while (count < newPolyExp.length && exponents[i] != newPolyExp[count])
+				count++;
+
+			newPolyConst[count] = constants[i];
+
+		}
+
+		for (int i = 0; i < input.exponents.length; i++) {
+
+			count = 0;
+
+			while (count < newPolyExp.length && input.exponents[i] != newPolyExp[count])
+				count++;
+
+			newPolyConst[count] += input.constants[i];
+
+		}
+
+		return new Polynomial(newPolyConst, newPolyExp);
+
+	}
+
+	// takes one argument of type double representing a value of x and evaluates the
+	// polynomial accordingly
+	public double evaluate(double x) {
+
+		double total = 0;
+
+		for (int i = 0; i < constants.length; i++) {
+
+			total += constants[i] * (Math.pow(x, exponents[i]));
+		}
+
 		return total;
 	}
-	
-	//takes one argument of type double and determines whether this value is a root of the polynomial or not
+
+	// takes one argument of type double and determines whether this value is a root
+	// of the polynomial or not
 	public boolean hasRoot(double x) {
-		
+
 		return evaluate(x) == 0;
-		
+
 	}
-	
-	
+
+	public Polynomial multiply(Polynomial input) {
+
+		if (constants == null || input.constants == null) {
+
+			return new Polynomial();
+		}
+
+		int len1 = constants.length;
+		int len2 = input.constants.length;
+
+		double[] newConst = new double[len1 * len2];
+		int[] newExp = new int[len1 * len2];
+
+		int k = 0;
+
+		for (int i = 0; i < len1; i++) {
+
+			for (int j = 0; j < len2; j++) {
+
+				newConst[k] = constants[i] * input.constants[j];
+				newExp[k] = exponents[i] + input.exponents[j];
+
+				k++;
+			}
+		}
+
+		// find # of distinct values in newExp[]
+		int maxExp = 0;
+
+		for (int i = 0; i < newExp.length; i++) {
+
+			if (newExp[i] > maxExp)
+				maxExp = newExp[i];
+
+		}
+		int total = 0;
+		int count = 0;
+
+		for (int i = 0; i <= maxExp; i++) {
+
+			count = 0;
+			for (int j = 0; j < newExp.length; j++) {
+
+				if (newExp[j] == i)
+					count++;
+
+			}
+
+			if (count > 0)
+				total++;
+
+		}
+
+		double[] newPolyConst = new double[total];
+		int[] newPolyExp = new int[total];
+
+		k = 0;
+		boolean check = false;
+
+		for (int i = 0; i <= maxExp; i++) {
+
+			check = false;
+			for (int j = 0; j < newExp.length; j++) {
+
+				if (newExp[j] == i) {
+
+					newPolyConst[k] += newConst[j];
+					newPolyExp[k] = i;
+					check = true;
+
+				}
+			}
+			if (check)
+				k++;
+		}
+
+		return new Polynomial(newPolyConst, newPolyExp);
+
+	}
+
+	public void saveToFile(String name) throws IOException {
+
+		File file = new File(name);
+
+		if (file.exists() == false) {
+			file.createNewFile();
+		}
+
+		FileWriter output = new FileWriter(name);
+		int i = 0;
+
+		output.write(Double.toString(constants[i]));
+		if (exponents[i] != 0) {
+			output.write("x");
+			output.write(Integer.toString(exponents[i]));
+		}
+
+		for (i = 1; i < constants.length; i++) {
+			if (constants[i] > 0) {
+				output.write("+");
+			}
+
+			output.write(Double.toString(constants[i]));
+
+			if (exponents[i] != 0) {
+				output.write("x");
+				output.write(Integer.toString(exponents[i]));
+			}
+		}
+
+		output.close();
+
+	}
 }
-
-
